891ea3717f201af43227126d5a261175163fa952
Erik Tamayo <erik.tamayohe@uanl.edu.mx>
2023-05-12 15:28:52 -0600

    Update display.php

--- display.php
+++ display.php
@@ -8,19 +8,34 @@ if(isset($_POST['displaySend'])){
         <th scope="col">Name</th>
         <th scope="col">Email</th>
         <th scope="col">Mobile</th>
-        <th scope="col">Operations</th>
+        <th scope="col">Place</th>
+        <th scope="col">Operation</th>
       </tr>
       </thead>';
-      $sql="Select from `crud`";
+      $sql="Select * from `crud`";
       $result=mysqli_query($con,$sql);
       while($row=mysqli_fetch_assoc($result)){
+          $id=$row['id'];
+          $name=$row['name'];
+          $email=$row['email'];
+          $mobile=$row['mobile'];
+          $place=$row['place'];
           $table.='<tr>
-          <th scope="row">1</th>
-          <td>Mark</td>
-          <td>Otto</td>
-          <td>@mdo</td>
-         </tr>';
+          <td scope="row">'.$id.'</td>
+          <td>'.$name.'</td>
+          <td>'.$email.'</td>
+          <td>'.$mobile.'</td>
+          <td>'.$place.'</td>
+          <td>
+          <button class="btn btn-dark">Update</button>
+          <button class="btn btn-danger" onclick="DeleteUser('.$id.')">Delete</button>
+          </td>
+          </tr>';
       } 
+      $table.='</table>';
+      echo $table;
+
 }
+
 
 ?>
